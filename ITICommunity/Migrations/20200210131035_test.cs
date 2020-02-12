96689c3c5b626a8ddbf2c32f0ea00c7c8165aa29
SaraElmaghraby <58309697+SaraElmaghraby@users.noreply.github.com>
2020-02-12 13:38:31 +0200

    Add project files.

---
+++ ITICommunity/Migrations/20200210131035_test.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITICommunity.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Branch",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    branchLocation = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branch", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Intake",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Intake = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Intake", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Track",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Track = table.Column<string>(maxLength: 200, nullable: true),
+                    IntakeID = table.Column<int>(nullable: true),
+                    BranchID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Track", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Track_Branch",
+                        column: x => x.BranchID,
+                        principalTable: "Branch",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Track_Intake",
+                        column: x => x.IntakeID,
+                        principalTable: "Intake",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: true),
+                    Email = table.Column<string>(maxLength: 50, nullable: true),
+                    Password = table.Column<string>(maxLength: 50, nullable: true),
+                    NationalId = table.Column<string>(maxLength: 50, nullable: true),
+                    UserName = table.Column<string>(maxLength: 50, nullable: true),
+                    IntakeID = table.Column<int>(nullable: true),
+                    trackID = table.Column<int>(nullable: true),
+                    About = table.Column<string>(nullable: true),
+                    CVFile = table.Column<byte[]>(nullable: true),
+                    ProfilePic = table.Column<string>(nullable: true),
+                    JobTitle = table.Column<string>(maxLength: 100, nullable: true),
+                    BirtghDay = table.Column<DateTime>(type: "date", nullable: true),
+                    UserTypeID = table.Column<int>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Intake",
+                        column: x => x.IntakeID,
+                        principalTable: "Intake",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_User_UserType",
+                        column: x => x.UserTypeID,
+                        principalTable: "UserType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Follow",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false),
+                    FollowingID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Follow", x => new { x.UserID, x.FollowingID });
+                    table.ForeignKey(
+                        name: "FK_Follow_User1",
+                        column: x => x.FollowingID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Follow_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostBody = table.Column<string>(nullable: true),
+                    Picture = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(type: "date", nullable: true),
+                    Video = table.Column<string>(nullable: true),
+                    UserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserContacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    ContactDetails = table.Column<string>(maxLength: 200, nullable: true),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    ContactTypeID = table.Column<int>(nullable: true),
+                    UserID = table.Column<int>(nullable: true),
+                    Hide = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserContacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserContacts_ContactType",
+                        column: x => x.ContactTypeID,
+                        principalTable: "ContactType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserContacts_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEducation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SchoolName = table.Column<string>(maxLength: 200, nullable: true),
+                    Degree = table.Column<string>(maxLength: 50, nullable: true),
+                    FieldOfStudy = table.Column<string>(maxLength: 50, nullable: true),
+                    Grade = table.Column<string>(maxLength: 50, nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(type: "date", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "date", nullable: true),
+                    UserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEducation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserEducation_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserWorkExperience",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 100, nullable: true),
+                    EmploymentType = table.Column<string>(maxLength: 100, nullable: true),
+                    CompanyName = table.Column<string>(maxLength: 100, nullable: true),
+                    CompanyLocation = table.Column<string>(maxLength: 100, nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(type: "date", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "date", nullable: true),
+                    UserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserWorkExperience", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserWorkExperience_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    CommentBody = table.Column<string>(nullable: true),
+                    UserID = table.Column<int>(nullable: true),
+                    PostID = table.Column<int>(nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comment_Post",
+                        column: x => x.PostID,
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comment_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Like",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(nullable: true),
+                    PostID = table.Column<int>(nullable: true),
+                    CommentID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Like", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Like_Comment",
+                        column: x => x.CommentID,
+                        principalTable: "Comment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Like_Post",
+                        column: x => x.PostID,
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Like_User",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_PostID",
+                table: "Comment",
+                column: "PostID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_UserID",
+                table: "Comment",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Follow_FollowingID",
+                table: "Follow",
+                column: "FollowingID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_CommentID",
+                table: "Like",
+                column: "CommentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_PostID",
+                table: "Like",
+                column: "PostID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_UserID",
+                table: "Like",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_UserID",
+                table: "Post",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Track_BranchID",
+                table: "Track",
+                column: "BranchID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Track_IntakeID",
+                table: "Track",
+                column: "IntakeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_IntakeID",
+                table: "User",
+                column: "IntakeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_UserTypeID",
+                table: "User",
+                column: "UserTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContacts_ContactTypeID",
+                table: "UserContacts",
+                column: "ContactTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContacts_UserID",
+                table: "UserContacts",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserEducation_UserID",
+                table: "UserEducation",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserWorkExperience_UserID",
+                table: "UserWorkExperience",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Follow");
+
+            migrationBuilder.DropTable(
+                name: "Like");
+
+            migrationBuilder.DropTable(
+                name: "Track");
+
+            migrationBuilder.DropTable(
+                name: "UserContacts");
+
+            migrationBuilder.DropTable(
+                name: "UserEducation");
+
+            migrationBuilder.DropTable(
+                name: "UserWorkExperience");
+
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "Branch");
+
+            migrationBuilder.DropTable(
+                name: "ContactType");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Intake");
+
+            migrationBuilder.DropTable(
+                name: "UserType");
+        }
+    }
+}
